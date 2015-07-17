38d732ea05afb5e294fe4b2bec1968e39e91a41c
Mandeep Singh <mandeeps708@gmail.com>
2015-07-18 04:43:49 +0530

    Update presentation.sh

--- presentation/presentation.sh
+++ presentation/presentation.sh
@@ -1,3 +1,6 @@
+#The purpose of this script is to take input from a file 'list', 
+#then find and replace the 'variable()'' with names fetched from file 'list'.
+
 #!/bin/bash
 let count=1
 while read line
@@ -5,3 +8,5 @@ do
 sed -i -e s/'variable('$count')'/$line/ filename
 ((count++))
 done<list
+
+# http://github.com/mandeeps708
