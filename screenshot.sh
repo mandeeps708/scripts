669a5c89f8ccbf0e3aca0bd979e2e4178d984d23
mandeeps708 <mandeeps708@gmail.com>
2015-08-07 22:11:14 +0530

    5 sec. wait

--- screenshot.sh
+++ screenshot.sh
@@ -3,7 +3,7 @@
 #storing them at a location say ~/Desktop/screenshots. This folder will 
 #be automatically created if there is not one. The path may be changed 
 #by changing the variable path below.
-
+#One may add keyboard shortcut to run this script in 'System Settings'.
 path=~/Desktop/screenshots
 mkdir -p $path
 sleep 5
