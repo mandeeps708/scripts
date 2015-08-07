cd2e09edaddf0ce3ac550689c379d253475b74f9
mandeeps708 <mandeeps708@gmail.com>
2015-08-07 23:12:05 +0530

    added comments

--- screenshot3.sh
+++ screenshot3.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-#This script takes the screenshot of current focused window and saves 
-#it in a folder say ~/Desktop/screenshots. This location can be modified 
-#by changing the path variable below.
+#This script takes the screenshot of area selected by two mouse clicks and 
+#saves it in a folder say ~/Desktop/screenshots. This location can be modified 
+#by changing the path variable below. Add shortcut to this script in the 
+#System Settings> Keyboard shortcuts.
 path=~/Desktop/screenshots
 mkdir -p $path
 gnome-screenshot --file=$path/Myscreenshot`date +%I.%M.%S`.png -B -a
