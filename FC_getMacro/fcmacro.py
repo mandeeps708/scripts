3c59e690a44bfbcb4deb561c4319eabd95f027d2
Mandeep Singh <mandeeps708@gmail.com>
2016-06-01 16:40:08 +0530

    ported for python2

--- FC_getMacro/fcmacro.py
+++ FC_getMacro/fcmacro.py
@@ -1,21 +1,17 @@
-import urllib.request
-
+from __future__ import print_function
 from pyquery import PyQuery as pq
-from lxml import etree
 
 """This script fetches the macro code from FreeCAD wiki.
 """
+
+macro_link = 'http://www.freecadweb.org/wiki/index.php?title=Macro_Global_Variable_Watcher'
+# Some other links for testing:
 # macro_link = 'http://www.freecadweb.org/wiki/index.php?title=Macro_Solid_Sweep'
 # macro_link = 'http://www.freecadweb.org/wiki/index.php?title=Macro_Image_Scaling'
-macro_link = 'http://www.freecadweb.org/wiki/index.php?title=Macro_Global_Variable_Watcher'
 
-html = urllib.request.urlopen(macro_link).read()
-d = pq(html)
-
-# print(html)
-# print('Macro link:',macro_link)
-# print('d:',d)
+jQuery = pq(url=macro_link)
+# print('jQuery: ', jQuery)
 
 # Parsed Macro code.
-macro_code = d('.mw-highlight.mw-content-ltr > pre').html()
-print('link:',macro_code)
+macro_code = jQuery('.mw-highlight.mw-content-ltr > pre').html()
+print(macro_code)
